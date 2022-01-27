01f6945f79cb0ca667b915ca722150ea92aa431a
ozkancondek <ozkncndek@gmail.com>
2022-01-27 17:34:29 +0100

    lesson-3:data types in function

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,26 @@
+//lesson 2*******************
+// const myName = "ozkan condek";
+// console.log(myName);
+// //get inputs
+// const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+//lesson 3************
+//we have only one number type
 var myName = "ozkan condek";
-console.log(myName);
-//get inputs
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 25;
+var condition = false;
+//myName = 20;  not allowed to change type but its allowed for the same type
+myName = "example";
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(1));
+//console.log(circ("hello"));   NuN
+var circ2 = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(5));
+//console.log(circ("hello"));   Nuot gonna work
--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,37 @@
-const myName = "ozkan condek";
-console.log(myName);
+//lesson 2*******************
 
-//get inputs
+// const myName = "ozkan condek";
+// console.log(myName);
 
-const inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach((input) => {
-  console.log(input);
-});
+// //get inputs
+
+// const inputs = document.querySelectorAll("input");
+// console.log(inputs);
+// inputs.forEach((input) => {
+//   console.log(input);
+// });
+
+//lesson 3************
+
+//we have only one number type
+
+let myName = "ozkan condek";
+let age = 25;
+let condition = false;
+
+//myName = 20;  not allowed to change type but its allowed for the same type
+myName = "example";
+
+const circ = (diameter) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(1));
+//console.log(circ("hello"));   NuN
+
+const circ2 = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(5));
+//console.log(circ("hello"));   Nuot gonna work
