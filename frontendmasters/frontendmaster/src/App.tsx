e2de2e53f225a6e7a6baed15e002242298ec64bd
ozkancondek <ozkncndek@gmail.com>
2022-02-01 23:16:08 +0100

    01:Project Structure created

---
+++ frontendmasters/frontendmaster/src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {" "}
+      <main>
+        <header>
+          <h1>Hello</h1>
+          <p>My Name Is</p>
+        </header>
+        <section className="display-name">
+          <p>[Your Name Here]</p>
+        </section>
+        <footer />
+      </main>
+    </div>
+  );
+}
+
+export default App;
