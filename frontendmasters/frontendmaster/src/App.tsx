cf9f22130aa138600bc101ea4c27bdc41383165d
ozkancondek <ozkncndek@gmail.com>
2022-02-02 23:17:38 +0100

    counter example with use effect

--- frontendmasters/frontendmaster/src/App.tsx
+++ frontendmasters/frontendmaster/src/App.tsx
@@ -4,17 +4,19 @@ import { useState } from "react";
 import { UseStateHook } from "./lectures/UseStateHook";
 
 import { questions } from "./lectures/questions";
+import { BrokenCounter } from "./lectures/BrokenCounter";
 
 function App() {
   const [name, setMyName] = useState("özkan"); //it should be a string
   return (
     <div className="App">
       {/*  <NameTag name={name} /> */}
-      <main>
+      {/*       <main>
         {questions.map((q) => (
           <UseStateHook question={q.question} answer={q.answer} key={q.id} />
         ))}
-      </main>
+      </main> */}
+      <BrokenCounter />
     </div>
   );
 }
