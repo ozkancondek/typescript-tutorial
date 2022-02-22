f9955315ab851befada9cd2a0acf8968a541da9f
ozkancondek <ozkncndek@gmail.com>
2022-02-22 20:14:52 +0100

    lesson 1, typescript beginner

--- yutube-typescript/src/lesson1/Lesson1.tsx
+++ yutube-typescript/src/lesson1/Lesson1.tsx
@@ -16,12 +16,30 @@ export const Lesson1 = () => {
   interface ForChannel {
     name: string;
     follower: number;
+    date?: string | number;
   }
 
   const channel3: ForChannel = {
     name: "react tutorial",
     follower: 4568,
   };
+  // channel3 need to have two determined keys and in given format
+  //but i can arrange it as optional. date ? : number examply
+
+  //function
+  const sumValues = (a: number, b: number) => a + b;
+  const result = sumValues(3, 5); // typescript now the type of result is a number
+
+  //define type for functions
+  type SumType = (x: number, y: number) => number;
+
+  const sumValues2: SumType = (x, y) => x + y;
+
+  //union type
+  type name = number | string;
+
+  //literal type
+  type name2 = "ozkan" | "ozkan2";
 
   return <div>Lesson1</div>;
 };
